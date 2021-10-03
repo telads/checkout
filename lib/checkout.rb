7fc8622419055d1340674eeefcdb6f43e1557354
telads <s.telads@gmail.com>
2021-10-03 09:16:40 +0100

    first four tests passing

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,7 +1,11 @@
-class Supermarket 
-    def checkout(goods)
-        if goods != (/[^a-zA-Z]/)
-            -1
-        end
+class Supermarket
+  def checkout(goods)
+     if goods.is_a? Integer 
+        -1
+     elsif goods != goods.upcase
+        -1 
+     else
+        100
+     end
     end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,9 +1,9 @@
 require 'checkout'
 
 describe 'Supermarket Checkout' do
-    it "throws an error for 'aBc'" do
-    supermarket = Supermarket.new
-    expect(supermarket.checkout('aBc')).to eq -1
+      it "throws an error for 'aBc'" do
+      supermarket = Supermarket.new
+      expect(supermarket.checkout('aBc')).to eq -1
     end
 
     it "throws an error for '-B8x'" do
@@ -14,6 +14,11 @@ describe 'Supermarket Checkout' do
     it "throws an error for 18" do
         supermarket = Supermarket.new
         expect(supermarket.checkout(18)).to eq -1
+    end
+
+    it "AA = 100" do
+        supermarket = Supermarket.new
+        expect(supermarket.checkout("AA")).to eq 100
     end
     
 end
